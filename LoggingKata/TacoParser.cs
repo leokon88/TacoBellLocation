9db1ee34cdcf7ab9642ed021a10dc2c276e523e0
YShin <yookyung.shin@gmail.com>
2019-07-08 16:27:39 -0600

    tacoParser class updated

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -10,9 +10,22 @@
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
+            var cells = line.Split(',');
 
+            
+
+            // If your array.Length is less than 3, something went wrong
+            if (cells.Length < 3)
+            {
+                return null; // TODO Implement
+                // Log that and return null
+            }
             // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            else
+            {
+                TacoBell taco = new TacoBell( double.Parse(cells[0]), double.Parse(cells[0]), cells[2].ToString());
+                return taco;
+            }
         }
     }
 }
