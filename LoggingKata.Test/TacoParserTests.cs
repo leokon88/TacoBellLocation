40bc0f25c301ac5bfb14bd2ea72508537e2b4caa
YShin <yookyung.shin@gmail.com>
2019-07-08 15:08:25 -0600

    test statements added

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -11,19 +11,50 @@ namespace LoggingKata.Test
             // TODO: Complete Something, if anything
         }
 
+
         [Theory]
-        [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("34.07,-84.67,Taco Bell Acwort", 34.07, -84.67, "Taco Bell Acwort")]
+        [InlineData(" 31,-84.17,Taco Bell Albany", 31.00, -84.17, "Taco Bell Albany")]
+        [InlineData(" 31,0,Taco Bell Albany", 31.00, 0.00, "Taco Bell Albany")]
+
+
+        public void ShouldParse(string line, object expectedResult)
         {
-            // TODO: Complete Should Parse
+
+            //Arrange
+            TacoParser tester = new TacoParser();
+
+            //Act
+
+            ITrackable actual = tester.Parse(line);
+
+            //Assert
+            Assert.Equal(expectedResult, actual);
+
         }
+     
 
         [Theory]
         [InlineData(null)]
         [InlineData("")]
+        [InlineData(" 31,Taco,Taco Bell Albany", "Invalid")]
+        //[InlineData("34.07,aaa,Taco Bell Acwort")]
         public void ShouldFailParse(string str)
         {
             // TODO: Complete Should Fail Parse
+            //Arrange
+            TacoParser tester = new TacoParser();
+            //Act
+
+            ITrackable actual = tester.Parse(str);
+            //Assert
+
+            Assert.Null(null);
+
+            Assert.Empty(str);
+
+
+
         }
     }
 }
